733e14d7a57321f05c6d9c534043351d22a199ff
Juliano Bressan <bressan.rs@gmail.com>
2021-03-28 11:25:19 -0300

    Added environment variables loading before executing program

--- public/index.php
+++ public/index.php
@@ -1,13 +1,19 @@
 <?php
+
+use ATCM\Data\Models\Aircraft;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 
 require __DIR__ . '/../vendor/autoload.php';
 
+$environmentVars = \Dotenv\Dotenv::createImmutable(__DIR__ . "/..");
+$environmentVars->load();
+
 $app = AppFactory::create();
 
 $app->get('/', function (Request $request, Response $response, $args) {
+    Aircraft::all();
     $response->getBody()->write("Hello world!");
     return $response;
 });
