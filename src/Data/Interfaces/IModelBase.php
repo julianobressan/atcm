cb4e556e505b15daec91eba2bab8537e01966151
Juliano Bressan <bressan.rs@gmail.com>
2021-03-28 19:40:54 -0300

    Added some support methods in the interface of models

--- src/Data/Interfaces/IModelBase.php
+++ src/Data/Interfaces/IModelBase.php
@@ -17,30 +17,29 @@ interface IModelBase
      * @param int $id The 
      * @return IModelBase An object that represent the related row in the database
      */
-    static function find(int $id);
+    static function find(int $id): ?IModelBase;
         
     /**
      * Deletes a row in database identified by your ID (primary key)
      *
-     * @param int $id
+     * @param mixed $id
      * @return void
      */
-    static function destroy(int $id);
+    static function destroy($objectOrId): void;
 
     /**
      * Deletes a row in database identified by your ID (primary key)
      *
-     * @param int $id
      * @return void
      */
-    function delete();
+    function delete(): void;
         
     /**
      * Updates or creates a row in the database
      *
      * @return IModelBase An object that represent the related row in the database
      */
-    function save();
+    function save(): IModelBase;
         
     /**
      * Creates an object with parameters passed by array
@@ -48,5 +47,34 @@ interface IModelBase
      * @param array $params A named array with fields for the object creation
      * @return IModelBase
      */
-    static function create(array $params);
+    static function create(array $params = []): IModelBase;
+
+    /**
+     * Return all registers in the table
+     *
+     * @param  mixed $filter Where clausules can be passed
+     * @param  mixed $limit Number of registers to return can be informed
+     * @param  mixed $offset Offset of registers can be passed, useful for pagination
+     * @return array
+     */
+    static function all(string $filter = '', array $orderBy=[], int $limit = 0, int $offset = 0): array;
+
+    /**
+     * Return all registers in the table
+     *
+     * @param  mixed $filter Where clausules can be passed
+     * @param  mixed $limit Number of registers to return can be informed
+     * @param  mixed $offset Offset of registers can be passed, useful for pagination
+     * @return IModelBase
+     */
+    static function first(string $filter = '', array $orderBy=[]): ?IModelBase;
+
+    /**
+     * Return the number of results in the table, acording the filter passed (default = empty)
+     *
+     * @param  string $fieldName
+     * @param  string $filter
+     * @return int Number of registers
+     */
+    static function count(string $fieldName = '*', string $filter = ''): int;
 }
