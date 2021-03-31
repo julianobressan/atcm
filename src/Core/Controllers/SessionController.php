635b4b73e8cac19d15438b64f82faad2c2ba9ae8
Juliano Bressan <bressan.rs@gmail.com>
2021-03-31 06:04:35 -0300

    Implementation of user validation

--- src/Core/Controllers/SessionController.php
+++ src/Core/Controllers/SessionController.php
@@ -2,8 +2,11 @@
 
 namespace ATCM\Core\Controllers;
 
+use ATCM\Core\Exceptions\InvalidParameterException;
+use ATCM\Core\Exceptions\NotAllowedException;
 use ATCM\Core\Exceptions\RestAPIException;
 use ATCM\Core\Helpers\ErrorHandlerHelper;
+use ATCM\Data\Models\User;
 use Firebase\JWT\JWT;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
@@ -17,12 +20,21 @@ class SessionController
             $login = $body['login'] ?? null;
             $password = $body['password'] ?? null;
 
+            if (empty($login) || empty($password)) {
+                throw new InvalidParameterException("Login and password are mandatory.", 9, 406);
+            }
+
+            $user = User::findByLogin($login);
+            if (!$user || !password_verify($password, $user->password)) {
+                throw new NotAllowedException("Login or password incorrect.", 10, 401);
+            }
+
             $nowSeconds = time();
 
             $token = array(
                 "user" => $login,
                 "iat" => $nowSeconds,
-                "exp" => $nowSeconds+($_ENV["JWT_EXPIRATION_TIME"] * 60),  // Maximum expiration time is one minute
+                "exp" => $nowSeconds + ($_ENV["JWT_EXPIRATION_TIME"] * 60),  // Maximum expiration time is one minute
                 "sub" => $login
             );
             
--- src/Data/Models/User.php
+++ src/Data/Models/User.php
@@ -2,11 +2,16 @@
 
 namespace ATCM\Data\Models;
 
+use ATCM\Data\Interfaces\IModelBase;
 use ATCM\Data\ORM\ModelBase;
 
 class User extends ModelBase
 {
     protected bool $timestamps = true;
 
-    
+    public static function findByLogin(string $login): ?IModelBase
+    {
+        $user = User::first("login LIKE '{$login}'");
+        return $user;
+    }
 }
